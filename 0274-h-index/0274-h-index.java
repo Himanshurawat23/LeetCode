b03f6699fda04f2d3408c1ecfc8eee2a5c819786
Himanshu Rawat <119286868+Himanshurawat23@users.noreply.github.com>
2024-12-18 03:00:41 +0530

    Time: 2 ms (70.26%), Space: 41.2 MB (93.40%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,7 +1,6 @@
 class Solution {
     public int hIndex(int[] cit) {
-               Arrays.sort(cit);
-        int max=0;
+        Arrays.sort(cit);
         int n=cit.length;
         for(int i=0;i<n;i++){
             if((n-i)<=cit[i]){
